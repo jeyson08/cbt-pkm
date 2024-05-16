5b8786ebc48980844182e933196453a83275a534
jeyson08 <jeyson.boursault@gmail.com>
2024-05-16 18:08:44 +0200

    animation du texte en js et animation stats en css

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,47 @@
+// ----------- DEPART (jusqu'aux choix) ------------ //
+
+// ----------- CONDUCTEUR ------------ //
+
 const conducteur = document.querySelector(".conducteur");
+let scriptArray = [
+  "Un DRACOLOSSE sauvage apparaît !",
+  "Que souhaitez-vous faire ?",
+];
+let wordIndex = 0;
+let letterIndex = 0;
+
+// console.log(scriptArray[1][0]);
+
+const createLetter = () => {
+  const letter = document.createElement("span");
+  conducteur.appendChild(letter);
+
+  letter.textContent = scriptArray[wordIndex][letterIndex];
+
+  setTimeout(() => {
+    // letter.remove();
+    conducteur.innerHTML = "";
+  }, 2500);
+};
+
+const loop = () => {
+  setTimeout(() => {
+    if (wordIndex >= scriptArray.length) {
+      wordIndex = 0;
+      letterIndex = 0;
+      //   loop();
+    } else if (letterIndex < scriptArray[wordIndex].length) {
+      createLetter();
+      letterIndex++;
+      loop();
+    } else {
+      wordIndex++;
+      letterIndex = 0;
+      setTimeout(() => {
+        loop();
+      }, 2500);
+    }
+  }, 10);
+};
+
+loop();
