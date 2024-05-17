5f14f3941194426a1c6c3d502f70313b1b0cedbc
jeyson08 <jeyson.boursault@gmail.com>
2024-05-17 11:05:32 +0200

    choix : attaquer / objets / retour

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,6 +9,7 @@ let scriptArray = [
 ];
 let wordIndex = 0;
 let letterIndex = 0;
+const attaquesPlayer = ["Laser glace", "Hydrocanon"];
 
 // console.log(scriptArray[1][0]);
 
@@ -24,12 +25,48 @@ const createLetter = () => {
   }, 2500);
 };
 
+const attachEventListeners = () => {
+  const attaquer = document.getElementById("attaquer");
+  const objets = document.getElementById("objets");
+
+  if (attaquer) {
+    attaquer.addEventListener("click", () => {
+      conducteur.innerHTML = `<p id="${attaquesPlayer[0]}">${attaquesPlayer[0]}</p>
+        <p id="${attaquesPlayer[1]}">${attaquesPlayer[1]}</p>
+        <p id="retour">Retour</p>`;
+      document.getElementById("retour").addEventListener("click", () => {
+        showInitialOptions();
+      });
+    });
+  }
+
+  if (objets) {
+    objets.addEventListener("click", () => {
+      conducteur.innerHTML = `<p id="potion">Potion</p>
+        <p id="retour">Retour</p>`;
+      document.getElementById("retour").addEventListener("click", () => {
+        showInitialOptions();
+      });
+    });
+  }
+};
+
+const showInitialOptions = () => {
+  conducteur.innerHTML = `<p id="attaquer">Attaquer</p>
+    <p id="objets">Objets</p>`;
+  attachEventListeners();
+};
+
 const loop = () => {
   setTimeout(() => {
     if (wordIndex >= scriptArray.length) {
-      wordIndex = 0;
-      letterIndex = 0;
+      //   wordIndex = 0;
+      //   letterIndex = 0;
       //   loop();
+      showInitialOptions();
+      conducteur.style.display = "flex";
+      conducteur.style.justifyContent = "space-around";
+      conducteur.style.alignItems = "center";
     } else if (letterIndex < scriptArray[wordIndex].length) {
       createLetter();
       letterIndex++;
